9617376c67d5b14ca397831456968053d440de29
mechamonkey22 <49624961+mechamonkey22@users.noreply.github.com>
2019-11-05 13:00:23 -0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/MainScreen.c" "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/MainScreen.c.obj"
+  "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/MenuScreen.c" "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/MenuScreen.c.obj"
+  "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/MonitorSize.c" "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/MonitorSize.c.obj"
+  "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/NewFile.c" "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/NewFile.c.obj"
+  "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/UserLogin.c" "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/UserLogin.c.obj"
+  "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/UserRegister.c" "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/UserRegister.c.obj"
+  "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/main.c" "C:/Users/joao_vitor/CLionProjects/AlgoritmoTrab/cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../iup/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AlgoritmoTrab.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "AlgoritmoTrab.exe"
+  "AlgoritmoTrab.exe.manifest"
+  "AlgoritmoTrab.pdb"
+  "CMakeFiles/AlgoritmoTrab.dir/MainScreen.c.obj"
+  "CMakeFiles/AlgoritmoTrab.dir/MenuScreen.c.obj"
+  "CMakeFiles/AlgoritmoTrab.dir/MonitorSize.c.obj"
+  "CMakeFiles/AlgoritmoTrab.dir/NewFile.c.obj"
+  "CMakeFiles/AlgoritmoTrab.dir/UserLogin.c.obj"
+  "CMakeFiles/AlgoritmoTrab.dir/UserRegister.c.obj"
+  "CMakeFiles/AlgoritmoTrab.dir/main.c.obj"
+  "libAlgoritmoTrab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AlgoritmoTrab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
